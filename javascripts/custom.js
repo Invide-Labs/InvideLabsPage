140016f6fd1a7cffeaf0ab5721f1e59558bf5015
orderchhotu <orderchhotu@gmail.com>
2017-07-23 23:18:46 +0530

    minor styling changes, loader

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -3,3 +3,16 @@ $(document).ready(function(){
 	  window.location.href = $(this).find("option:selected").attr("id") + '.html';
 	});
 });
+
+document.onreadystatechange = function () {
+  var state = document.readyState
+  if (state == 'interactive') {
+       document.getElementById('contents').style.visibility="hidden";
+  } else if (state == 'complete') {
+      setTimeout(function(){
+         document.getElementById('interactive');
+         document.getElementById('load').style.visibility="hidden";
+         document.getElementById('fullpage').style.visibility="visible";
+      },1000);
+  }
+}
