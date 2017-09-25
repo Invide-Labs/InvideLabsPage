dfb76dfc314c592f4949a9c711b80607bdc237eb
orderchhotu <orderchhotu@gmail.com>
2017-09-25 16:34:31 +0530

    improving the content for better communication

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 document.onreadystatechange = function () {
   var state = document.readyState
   if (state == 'interactive') {
-       document.getElementById('contents').style.visibility="hidden";
+       document.getElementById('fullpage').style.visibility="hidden";
   } else if (state == 'complete') {
       setTimeout(function(){
          document.getElementById('interactive');
